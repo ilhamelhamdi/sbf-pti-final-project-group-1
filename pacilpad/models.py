a39edf590e88d8a6a0f898bd245d52c08b33a599
ilhamelhamdi <ilhamabdillah123@gmail.com>
2022-11-27 23:56:35 +0700

    Added validator to Book models

--- pacilpad/models.py
+++ pacilpad/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MinValueValidator
 
 class Writer(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.EmailField()
 
 class Publisher(models.Model):
     name = models.CharField(max_length=255)
@@ -16,8 +17,8 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     writer = models.ForeignKey(Writer, on_delete=models.CASCADE)
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
-    isbn = models.IntegerField()
+    isbn = models.CharField(max_length=13, validators=[MinLengthValidator(13)])
     synopsis = models.TextField()
-    page = models.IntegerField()
-    cover = models.ImageField()
+    page = models.IntegerField(validators=[MinValueValidator(1)])
+    cover = models.ImageField(upload_to='upload')
     date = models.DateField()
